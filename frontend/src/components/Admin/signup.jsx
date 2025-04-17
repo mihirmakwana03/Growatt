120b6237c2ae70e7e3d2034bf5e07707d081fb75
Mihir Makwana <mihirpmakwana786@gmail.com>
2025-04-18 03:58:36 +0530

    Updated design of Admin Dashboard

--- frontend/src/components/Admin/signup.jsx
+++ frontend/src/components/Admin/signup.jsx
@@ -58,7 +58,7 @@ const Signup = () => {
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-300">
             <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
-                <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">Register Admin</h1>
+                <h1 className="text-2xl font-bold text-center text-blue-600 mb-6">Register Admin</h1>
                 <form onSubmit={handleSubmit}>
                     <div className="mb-4">
                         <label htmlFor="username" className="block text-sm font-medium text-gray-700">
