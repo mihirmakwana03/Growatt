f8187b3217d42b542c87e7d92eeed27a06c941f5
Mihir Makwana <mihirpmakwana786@gmail.com>
2025-04-18 03:57:54 +0530

    Updated design of Admin Dashboard

--- frontend/src/components/Admin/adProfile.jsx
+++ frontend/src/components/Admin/adProfile.jsx
@@ -54,7 +54,7 @@ const AdProfile = () => {
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-300">
             <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
-                <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">Admin Profile</h1>
+                <h1 className="text-2xl font-bold text-center text-blue-600 mb-6">Admin Profile</h1>
                 <form onSubmit={handleSubmit} className="space-y-4 flex flex-col items-center justify-center">
                     <div className="flex justify-center">
                         <img
